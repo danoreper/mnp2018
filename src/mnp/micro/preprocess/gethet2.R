d9e31f6e75e04d069d93cf3d158b5fa553e19a9a
Daniel Oreper <doreper@alum.mit.edu>
2017-12-14 17:22:18 -0500

    a

--- src/mnp/micro/preprocess/gethet2.R
+++ src/mnp/micro/preprocess/gethet2.R
@@ -3,18 +3,18 @@ source("./genomerep/variantdb2/dump_parser.R")
 dbdir = "../../data/isvdb/full_1410"
 db    = db_builder$get.db.lite(dbdir)
 
-## nodhet = list()
-## for(chr in db$genotype$getChrs())
-## {
-##     nodhet[[chr]] = db$read(strain1 = "NOD_ShiLtJ", strain2 = "C57BL6J", type = "genotype", phased = T, chr = chr)
-## }
+nodhet = list()
+for(chr in db$genotype$getChrs())
+{
+    nodhet[[chr]] = db$read(strain1 = "NOD_ShiLtJ", strain2 = "C57BL6J", type = "genotype", phased = T, chr = chr)
+}
 
-## nodhet = do.call(rbind, nodhet)
-## nodhet = nodhet[allele1!="None"]
-## nodhet = nodhet[allele1!=allele2]
-## fwrite(nodhet, "../data/mnp/d_b6_variants.csv")
-## rm("nodhet")
-## gc()
+nodhet = do.call(rbind, nodhet)
+nodhet = nodhet[allele1!="None"]
+nodhet = nodhet[allele1!=allele2]
+fwrite(nodhet, "../data/mnp/d_b6_variants.csv")
+rm("nodhet")
+gc()
 
 
 hets = list()
