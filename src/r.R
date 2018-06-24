5c0a68869269bd4eef9b4ee9088941e7be15b3bd
Daniel Oreper <doreper@email.unc.edu>
2018-06-24 14:14:02 -0400

    a

--- src/r.R
+++ src/r.R
@@ -24,10 +24,38 @@ run <- function()
     ##                             inp$karyo)
 
 ##    source("./mnp/behavior/cleanData.R");
-    source("mnp/behavior/loadData.R");
-    source("mnp/behavior/analysis.R");
-    phenz = loadBehavior$getPhenotypeRepository();
-    beh.analysis$runAll(phenz)
+    first = F
+    source("./mnp/behavior/loadData.R");
+    source("./mnp/behavior/analysis.R");
+    source("./mnp/loadAllData.R")
+    source("./mnp/behavior/analysis.R")
+    source("./mnp/micro/analysis.R")
+    source("./mnp/micro/report.R")
+    source("./mnp/qpcr/analysis.R")
+    if(first)
+    {
+        load(outm(fp("micro","perm.RData")))
+        inp  = loadAllData$createAllInputs()
+    }
+    source("./mnp/behavior/loadData.R");
+    source("./mnp/behavior/analysis.R");
+    source("./mnp/loadAllData.R")
+    source("./mnp/behavior/analysis.R")
+    source("./mnp/micro/analysis.R")
+    source("./mnp/micro/report.R")
+    source("./mnp/qpcr/analysis.R")
+
+    micro.report$reportAnalysis(inp$exp.mat,
+                            inp$cov.data.full,
+                            out$ident.full$results,
+                            ##                            out$results$results,
+                            inp$annot.data,
+                            inp$probesetInfo,
+                            out$threshholds,
+                            inp$karyo)
+    
+    ## phenz = loadBehavior$getPhenotypeRepository();
+    ## beh.analysis$runAll(phenz)
 }
 
 
